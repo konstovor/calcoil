2877be316edda96d875148d9e680d1b3a2d9b9f2
kosovo <konsto-vor@yandex.ru>
2026-07-04 23:04:53 +0300

    0.1
    just ui

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui_main import *
+
+
+class MyWin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWin()
+    myapp.show()
+    sys.exit(app.exec_())
